ebfc67925d26bc637881e208a124653133199d13
Clement Petit <freedonaab@gmail.com>
2016-12-10 00:40:55 +0100

    test: Fix webpack unused loader

--- webpack/webpack.test.js
+++ webpack/webpack.test.js
@@ -4,8 +4,6 @@ module.exports = function (env, conf) {
 	const path = require('path');
 	const webpack = require('webpack');
 	const DefinePlugin = require('webpack/lib/DefinePlugin');
-	const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
-	const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 	const rootDir = path.resolve(__dirname, '..');
 	const app = 'app';
@@ -16,8 +14,7 @@ module.exports = function (env, conf) {
 		devtool: 'inline-source-map',
 		entry: {
 			app: [path.resolve(rootDir, app, 'main')],
-			vendor: [path.resolve(rootDir, app, 'vendor')],
-			css: [ path.resolve(rootDir, app, 'app-module.scss')]
+			vendor: [path.resolve(rootDir, app, 'vendor')]
 		},
 		module: {
 			preLoaders: [
@@ -36,34 +33,6 @@ module.exports = function (env, conf) {
 					test: /\.ts$/,
 					exclude: /node_modules/
 				},
-				{
-					test: /\.scss$/,
-					loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
-				},
-				{
-					test: /\.(png|jpe?g|gif|ico|svg)$/,
-					loader: 'file-loader?name=[name].[hash].[ext]'
-				},
-				{
-					test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-					loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-				},
-				{
-					test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-					loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-				},
-				{
-					test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-					loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-				},
-				{
-					test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-					loader: 'file-loader'
-				},
-				{
-					test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-					loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-				}
 			]
 		},
 		output: {
