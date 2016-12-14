db7c3222fd4c98f1ae3165f1fdf0ce8c2b890722
Clement Petit <clement.petit@neo9.fr>
2016-12-14 13:33:14 +0100

    header: Change text on login

--- app/header/header-component.ts
+++ app/header/header-component.ts
@@ -1,12 +1,17 @@
 import {Component} from "@angular/core";
-import {ConfigurationService} from "../components/conf/configuration-service";
+import {SessionService, Profile, Session} from "../components/session/session-service";
 
 @Component({
 	selector: 'header',
 	templateUrl: './header.html'
 })
 export class HeaderComponent {
-	constructor(private conf: ConfigurationService) {
+	profile: Profile;
+	constructor(private session: SessionService) {
+		this.session.session$.subscribe(s => this.onProfileChange(s));
+	}
 
+	onProfileChange(session: Session) {
+		this.profile = session ? session.profile : null;
 	}
 }
