d7f7e8692648d4aacedcd52380024e4f02d0adeb
Clement Petit <freedonaab@gmail.com>
2016-12-18 17:26:11 +0100

    sessions: Fix logged in guard

--- app/components/session/logged-in-guard.ts
+++ app/components/session/logged-in-guard.ts
@@ -10,12 +10,15 @@ export class LoggedInGuard implements CanActivate {
 	}
 
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
-		// console.log('can activate');
-		return this.session.islogged$.map(isLogged => {
-			if (isLogged) return true;
-			this.router.navigate(['login']);
-			return false;
-		});
+		return this.session.islogged$
+			.map(isLogged => {
+				if (isLogged) {
+					return true;
+				}
+				this.router.navigate(['login']);
+				return false;
+			})
+			.take(1);
 	}
 
 }
