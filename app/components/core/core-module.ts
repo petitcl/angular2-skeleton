99f0330a2a81c7d7958d75063c584db7dafe89e3
Clement Petit <freedonaab@gmail.com>
2016-12-23 23:50:56 +0100

    core: Prevent multiple inclusion

--- app/components/core/core-module.ts
+++ app/components/core/core-module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from "@angular/core";
+import {NgModule, SkipSelf, Optional} from "@angular/core";
 import {ConfigurationModule} from "../conf/configuration-module";
 import {StoreModule} from "../store/store-module";
 import {SessionService} from "../session/session-service";
@@ -22,5 +22,11 @@ import {LoggedInGuard} from "../session/logged-in-guard";
 	]
 })
 export class CoreModule {
+
+	constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+		if (parentModule) {
+			throw new Error("CoreModule has already been loaded. Import Core modules in the AppModule only.");
+		}
+	}
 
 }
