44fe130014efc77a8b67c15fd0b78df488793c46
Clement Petit <clement.petit@neo9.fr>
2017-03-01 15:18:27 +0100

    app: Add polyfill for intl

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,6 +1,8 @@
 import 'es6-shim';
 import 'es7-shim';
 import 'web-animations-js';
+import 'intl';
+import 'intl/locale-data/jsonp/fr.js';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
